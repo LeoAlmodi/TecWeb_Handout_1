e8f64264b6e9ce602d9c4f29be2a6e66883a2b67
Leonardo <leonardoar2@al.insper.edu.br>
2024-02-08 08:52:57 -0300

    parte 3

--- servidor.py
+++ servidor.py
@@ -1,10 +1,16 @@
 import socket
 from pathlib import Path
-from utils import extract_route, read_file
+from utils import extract_route, read_file, load_data
 
 CUR_DIR = Path(__file__).parent
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 8080
+
+NOTE_TEMPLATE = '''  <li>
+    <h3>{title}</h3>
+    <p>{details}</p>
+  </li>
+'''
 
 RESPONSE_TEMPLATE = '''HTTP/1.1 200 OK
 
@@ -20,34 +26,7 @@ RESPONSE_TEMPLATE = '''HTTP/1.1 200 OK
 <p>Como o Post-it, mas com outro verbo</p>
 
 <ul>
-  <li>
-    <h3>Receita de miojo</h3>
-    <p>Bata com um martelo antes de abrir o pacote. Misture o tempero, coloque em uma vasilha e aproveite seu snack :)</p>
-  </li>
-  <li>
-    <h3>Pão doce</h3>
-    <p>Abra o pão e coloque o seu suco em pó favorito.</p>
-  </li>
-  <li>
-    <h3>Sorvete com cristais de leite</h3>
-    <p>Sirva o seu sorvete favorito em uma vasilha e jogue leite em cima.</p>
-  </li>
-  <li>
-    <h3>Iogurte natural</h3>
-    <p>Deixe o leite fora da geladeira (esse é mentira, não faça isso).</p>
-  </li>
-  <li>
-    <h3>Homer Simpson</h3>
-    <p>~( 8(|)</p>
-  </li>
-  <li>
-    <h3>Numero mágico</h3>
-    <p>142857</p>
-  </li>
-  <li>
-    <h3>Série da Fundação - Isaac Asimov</h3>
-    <p>É boa, leia.</p>
-  </li>
+{notes}
 </ul>
 
 </body>
@@ -73,7 +52,15 @@ while True:
     if filepath.is_file():
         response = 'HTTP/1.1 200 OK\n\n'.encode() + read_file(filepath)
     else:
-        response = RESPONSE_TEMPLATE.encode()
+        # Cria uma lista de <li>'s para cada anotação
+        # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+        notes_li = [
+            NOTE_TEMPLATE.format(title=dados['titulo'], details=dados['detalhes'])
+            for dados in load_data('notes.json')
+        ]
+        notes = '\n'.join(notes_li)
+
+        response = RESPONSE_TEMPLATE.format(notes=notes).encode()
     client_connection.sendall(response)
 
     client_connection.close()
