20392f30d81dcb4d878455c2fe761f7fbfda7756
Leonardo <leonardoar2@al.insper.edu.br>
2024-02-08 11:12:41 -0300

    quase terminado

--- utils.py
+++ utils.py
@@ -17,3 +17,21 @@ def load_template(arquivo):
     op = Path("templates")/arquivo
     with open(op, "r") as file:
         return file.read()
+
+def add(anotacao):
+    op = Path("data")/"notes,json"
+    if op.exists():
+        with open(op, "r") as file:
+            data = json.load(file)
+    else:
+        data = []
+    data.append(anotacao)
+    with open(op, 'w') as file:
+        json.dump(data, file)
+
+def build_response(body="", code=200, reason="OK", headers=""):
+    if headers == "":
+        string = f"HTTP/1.1 {code} {reason}\n\n{body}"
+    else:
+        string = f"HTTP/1.1 {code} {reason}\n{headers}\n\n{body}"
+    return string.encode()
