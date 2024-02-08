9627f573160731f82a9a39ce924e4fece4b8f998
Leonardo <leonardoar2@al.insper.edu.br>
2024-02-08 09:22:46 -0300

    parte 3 concluida

--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
+from pathlib import Path
+import json
+
 def extract_route(requisicao):
     return requisicao.split()[1][1:]
 
 def read_file(argumento):
     with open(argumento, "rb") as file:
+        return file.read()
+
+def load_data(arquivo):
+    op = Path("data")/arquivo
+    with open(op, "r") as file:
+        return json.load(file)
+
+def load_template(arquivo):
+    op = Path("templates")/arquivo
+    with open(op, "r") as file:
         return file.read()
